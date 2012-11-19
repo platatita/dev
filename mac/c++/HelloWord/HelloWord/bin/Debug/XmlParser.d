a705d8366fe1fa7a4159aac489d2fbb428cf5fdb
Marcin Szewczyk <ms@anixe.pl>
2012-11-19 22:01:49 +0100

    Add new version of xmldiff

--- mac/c++/HelloWord/HelloWord/bin/Debug/XmlParser.d
+++
@@ -1,4 +0,0 @@
- \
- /Users/admin/sources/git/dev/mac/c++/HelloWord/HelloWord/bin/Debug/XmlParser.o:  \
- /Users/admin/sources/git/dev/mac/c++/HelloWord/HelloWord/XmlParser.cpp \
-  /Users/admin/sources/git/dev/mac/c++/HelloWord/HelloWord/XmlParser.h
--- mac/c++/HelloWord/HelloWord/bin/Debug/main.d
+++
@@ -1,4 +0,0 @@
- \
- /Users/admin/sources/git/dev/mac/c++/HelloWord/HelloWord/bin/Debug/main.o:  \
- /Users/admin/sources/git/dev/mac/c++/HelloWord/HelloWord/main.cpp \
-  /Users/admin/sources/git/dev/mac/c++/HelloWord/HelloWord/XmlParser.h
