8c9855c3c5a43c51522ad5f4e6cc65a152897bb7
Marcin Szewczyk <platatita@gmail.com>
2012-12-14 20:46:40 +0100

    Refactoring - split long code to small methods

--- mac/net/XmlDiff/XmlDiff/XmlConverter.cs
+++ mac/net/XmlDiff/XmlDiff/XmlConverter.cs
@@ -8,23 +8,23 @@ namespace XmlDiff
 	internal class XmlConverter : BaseReader
 	{
 		private readonly string xml;
-		
+		private readonly Root root;
+		private readonly Stack<Node> queue;
+		private Node parent;
+		private Node lastNode;
+		private int lastDepth;
+
 		internal XmlConverter (string xml)
 			: base()
 		{
 			this.xml = xml;
+			this.root = new Root();
+			this.queue = new Stack<Node>();
 		}
 		
 		internal List<Node> Convert()
 		{
-			Root root = new Root();
-
-			Stack<Node> queue = new Stack<Node>();
-			queue.Push (root);
-
-			Node parent = null;
-			Node lastNode = null;
-			int lastDepth = 0;
+			Initialize ();
 			
 			using (StringReader sr = new StringReader(this.xml))
 			{
@@ -36,31 +36,12 @@ namespace XmlDiff
 		            {
 		                if (base.IsStartElement())
 		                {
-							if (base.xmlReader.Depth == 0)
-							{
-								parent = root;
-							}
-							else if (lastDepth < base.xmlReader.Depth)
-							{
-								queue.Push(lastNode);								
-								parent = lastNode;
-							}
+							SetParent ();
 
-							Node currentNode = new Node(parent);
-							lastNode = currentNode;
-							
-							lastDepth = currentNode.Depth = base.xmlReader.Depth;
-							currentNode.Name = base.xmlReader.Name;
+							Node currentNode = CreateNode ();							
 
-							if (base.xmlReader.HasAttributes)
-							{
-								ReadAttributes(currentNode);
-							}
-
-							if (base.xmlReader.HasValue)
-							{
-								currentNode.Text = base.xmlReader.Value;
-							}
+							ReadAttributes(currentNode);
+							SetNodeText (currentNode);
 							
 							nodeCollection.Add(currentNode);
 						}
@@ -77,18 +58,62 @@ namespace XmlDiff
 			}
 		}
 
+		private void Initialize ()
+		{
+			this.queue.Clear ();
+			this.queue.Push (root);
+
+			this.parent = null;
+			this.lastNode = null;
+			this.lastDepth = 0;
+		}
+
+		private void SetParent ()
+		{
+			if (base.xmlReader.Depth == 0)
+			{
+				this.parent = this.root;
+			} 
+			else if (lastDepth < base.xmlReader.Depth)
+			{
+				this.queue.Push (this.lastNode);
+				this.parent = this.lastNode;
+			}
+		}
+
+		private Node CreateNode ()
+		{
+			Node currentNode = new Node (this.parent);
+			this.lastNode = currentNode;
+			this.lastDepth = currentNode.Depth = base.xmlReader.Depth;
+			currentNode.Name = base.xmlReader.Name;
+
+			return currentNode;
+		}
+
 		private void ReadAttributes (Node node)
 		{
-			node.AttributeCollection = new Dictionary<string, string>();
-			
-			for(int i = 0; i < base.xmlReader.AttributeCount; i++)
+			if (base.xmlReader.HasAttributes)
 			{
-				base.xmlReader.MoveToAttribute(i);
+				node.AttributeCollection = new Dictionary<string, string>();
 				
-				node.AttributeCollection.Add(base.xmlReader.Name, base.xmlReader.GetAttribute(i));
+				for(int i = 0; i < base.xmlReader.AttributeCount; i++)
+				{
+					base.xmlReader.MoveToAttribute(i);
+					
+					node.AttributeCollection.Add(base.xmlReader.Name, base.xmlReader.GetAttribute(i));
+				}
+				
+				base.xmlReader.MoveToElement();
 			}
-			
-			base.xmlReader.MoveToElement();
+		}
+
+		private void SetNodeText (Node currentNode)
+		{
+			if (base.xmlReader.HasValue) 
+			{
+				currentNode.Text = base.xmlReader.Value;
+			}
 		}
 	}
 }
